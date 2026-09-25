af9c82d6c8884fa415e5f08a4581afdc588bc5ea
School <jackdalton@Jacks-MacBook-Air.local>
2026-09-25 11:32:14 +0100

    added contact page

--- hello.py
+++ hello.py
@@ -1,8 +1,14 @@
-from flask import Flask
+from flask import Flask, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, World!"
+    return f'Hello, World! <a href="{url_for("contact")}">Contact</a>'
+
+
+@app.route('/contact')
+def contact():
+    return "Contact me at C24522069@mytudublin.ie"
+
 
