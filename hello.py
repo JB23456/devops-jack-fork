f76d2e192364583e6f24d31c50de7b5877711fae
Jack <c24522069@mytudublin.ie>
2026-09-25 11:38:29 +0100

    Change greeting message from 'Hello' to 'Greeting'

--- hello.py
+++ hello.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return f'Hello, World! <a href="{url_for("contact")}">Contact</a>'
+    return f'Greeting <a href="{url_for("contact")}">Contact</a>'
 
 
 @app.route('/contact')
